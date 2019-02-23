68f8ec6641a633f8cbf508a5e79934243316f72d
Pete Thompson <me@petethompson.com>
2019-02-23 20:54:22 +0000

    Add implementation for deromanise function

--- src/deromanise.js
+++ src/deromanise.js
@@ -1,5 +1,22 @@
+import numerals from "./numerals";
+
 function deromanise(string) {
-  return;
+  let result = 0;
+  let remaining = string.split("").reverse();
+  const isNegative = remaining[remaining.length - 1] === "-";
+
+  if (isNegative) remaining.pop();
+
+  while (remaining.length) {
+    const numeral = remaining.pop();
+    const nextNumeral = remaining[remaining.length - 1];
+    const arabic = numerals[numeral];
+    const isSubtractive = arabic < numerals[nextNumeral];
+
+    result = isSubtractive ? result - arabic : result + arabic;
+  }
+
+  return isNegative ? -result : result;
 }
 
 export default deromanise;
--- src/romanise.js
+++ src/romanise.js
@@ -1,24 +1,9 @@
+import numerals from "./numerals";
+
 function romanise(number) {
   if (number >= 4000 || number <= -4000 || number === 0) return;
 
   const isNegative = Math.sign(number) === -1;
-
-  // numerals for additive and subtractive representation
-  const numerals = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1
-  };
 
   let result = isNegative ? "-" : "";
   let remaining = Math.abs(number);

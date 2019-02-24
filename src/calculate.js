9f4b9f231fa4212a1a657d0f741781a668e21ae1
Pete Thompson <me@petethompson.com>
2019-02-24 13:23:39 +0000

    Add order implementation

--- src/calculate.js
+++ src/calculate.js
@@ -3,7 +3,7 @@ import deromanise from "./deromanise";
 
 const pattern = {
   numerals: "(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})",
-  symbols: "[+-/*]"
+  symbols: "[+-/*^]"
 };
 
 function isNumeral(string) {
@@ -11,17 +11,34 @@ function isNumeral(string) {
   return regex.test(string);
 }
 
+function isOrder(string) {
+  return string === "^";
+}
+
 function calculate(string) {
   const regex = new RegExp(`${pattern.numerals}|${pattern.symbols}`, "g");
 
-  // TODO: more validation of sanitised
-  const sanitised = string
+  const parts = string
     .match(regex)
-    .map(part => (isNumeral(part) ? deromanise(part) : part))
-    .join(" ");
+    .map(part => (isNumeral(part) ? deromanise(part) : part));
+
+  // TODO: more validation of sanitised
+  const sanitised = [];
+
+  while (parts.length) {
+    const part = parts.shift();
+
+    if (isOrder(part)) {
+      const prev = sanitised.pop();
+      const next = parts.shift();
+      sanitised.push(`Math.pow(${prev},${next})`);
+    } else {
+      sanitised.push(part);
+    }
+  }
 
   // TODO: some validation of arabic
-  const arabic = new Function(`return ${sanitised}`)();
+  const arabic = new Function(`return ${sanitised.join(" ")}`)();
 
   return romanise(arabic);
 }

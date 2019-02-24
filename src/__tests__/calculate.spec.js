57085a84304fa5901061beead5725315c2dc12d7
Pete Thompson <me@petethompson.com>
2019-02-24 12:18:42 +0000

    Add a few more tests for addition

--- src/__tests__/calculate.spec.js
+++ src/__tests__/calculate.spec.js
@@ -2,7 +2,12 @@ import calculate from "../calculate";
 
 describe("calculate", () => {
   it("should perform addition", () => {
-    const scenarios = [{ string: "XXIV + XI", result: "XXXV" }];
+    const scenarios = [
+      { string: "XXIV + XI", result: "XXXV" },
+      { string: "MM + MCMXCIX", result: "MMMCMXCIX" },
+      { string: "DC + LX + VI", result: "DCLXVI" },
+      { string: "I + II + III", result: "VI" }
+    ];
 
     scenarios.forEach(({ string, result }) =>
       expect(calculate(string)).toEqual(result)
